ee3085a0d5e4a1de08bf394966e64da25a4657ed
Matthew Kitt <mk@mkitt.net>
2017-07-11 15:59:50 -0600

    See if we have better luck with express
    Not built all the way through just testing to see if this works on
    heroku.

--- server.js
+++ server.js
@@ -1,42 +1,50 @@
-const { createServer } = require('http')
-const { parse } = require('url')
+const express = require('express')
 const next = require('next')
-const pathMatch = require('path-match')
+const bodyParser = require('body-parser')
 
-const dev = process.env.NODE_ENV !== 'production'
 const port = process.env.PORT || 3000
+const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
-const route = pathMatch()
-const matchProjects = route('/projects/:id')
-const matchServices = route('/services/:id')
+// const route = pathMatch()
+// const matchProjects = route('/projects/:id')
+// const matchServices = route('/services/:id')
 
-const customRoute = (pathname) => {
-  const serviceParams = matchServices(pathname)
-  if (serviceParams) {
-    return { params: serviceParams, path: '/services' }
-  }
-  const projectParams = matchProjects(pathname)
-  if (projectParams) {
-    return { params: projectParams, path: '/projects' }
-  }
-  return { params: null, route: null }
-}
+// const customRoute = (pathname) => {
+//   const serviceParams = matchServices(pathname)
+//   if (serviceParams) {
+//     return { params: serviceParams, path: '/services' }
+//   }
+//   const projectParams = matchProjects(pathname)
+//   if (projectParams) {
+//     return { params: projectParams, path: '/projects' }
+//   }
+//   return { params: null, route: null }
+// }
 
 app.prepare()
 .then(() => {
-  createServer((req, res) => {
-    const { pathname, query } = parse(req.url, true)
-    const props = customRoute(pathname)
-    if (props.params) {
-      app.render(req, res, props.path, Object.assign(props.params, query))
-      return
-    }
-    handle(req, res)
+  const server = express()
+  server.use(bodyParser.json())
+  server.use(bodyParser.urlencoded({ extended: true }))
+
+  server.get('/projects/:id', (req, res) => {
+    console.log('projects', req.query, req.url)
+    return app.render(req, res, '/projects', req.query)
   })
-  .listen(3000, (err) => {
+
+  server.get('/services/:id', (req, res) => {
+    console.log('services', req.query, req.url)
+    return app.render(req, res, '/services', req.query)
+  })
+
+  server.get('*', (req, res) => (
+    handle(req, res)
+  ))
+
+  server.listen(port, (err) => {
     if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`) // eslint-disable-line
+    console.log(`> RU Ready on http://localhost:${port}`) // eslint-disable-line
   })
 })
 
