4a613e506551458f5aacf42c16d3f953c1dc25b7
Matthew Kitt <mk@mkitt.net>
2017-07-11 16:57:49 -0600

    Setup and use express
    :fingers_crossed: this fixes stuff.

--- server.js
+++ server.js
@@ -1,26 +1,16 @@
 const express = require('express')
+const { parse } = require('url')
 const next = require('next')
 const bodyParser = require('body-parser')
+const pathMatch = require('path-match')
 
 const port = process.env.PORT || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
-// const route = pathMatch()
-// const matchProjects = route('/projects/:id')
-// const matchServices = route('/services/:id')
-
-// const customRoute = (pathname) => {
-//   const serviceParams = matchServices(pathname)
-//   if (serviceParams) {
-//     return { params: serviceParams, path: '/services' }
-//   }
-//   const projectParams = matchProjects(pathname)
-//   if (projectParams) {
-//     return { params: projectParams, path: '/projects' }
-//   }
-//   return { params: null, route: null }
-// }
+const route = pathMatch()
+const matchProjects = route('/projects/:id')
+const matchServices = route('/services/:id')
 
 app.prepare()
 .then(() => {
@@ -29,13 +19,15 @@ app.prepare()
   server.use(bodyParser.urlencoded({ extended: true }))
 
   server.get('/projects/:id', (req, res) => {
-    console.log('projects', req.query, req.url)
-    return app.render(req, res, '/projects', req.query)
+    const { pathname, query } = parse(req.url, true)
+    const params = matchProjects(pathname)
+    return app.render(req, res, '/projects', Object.assign(params, query))
   })
 
   server.get('/services/:id', (req, res) => {
-    console.log('services', req.query, req.url)
-    return app.render(req, res, '/services', req.query)
+    const { pathname, query } = parse(req.url, true)
+    const params = matchServices(pathname)
+    return app.render(req, res, '/services', Object.assign(params, query))
   })
 
   server.get('*', (req, res) => (
@@ -44,7 +36,7 @@ app.prepare()
 
   server.listen(port, (err) => {
     if (err) throw err
-    console.log(`> RU Ready on http://localhost:${port}`) // eslint-disable-line
+    console.log(`> Ready on http://localhost:${port}`) // eslint-disable-line
   })
 })
 
