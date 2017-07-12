efc1345812f1b4fb12067fe8215bcf495b1c9726
Matthew Kitt <mk@mkitt.net>
2017-07-11 19:14:47 -0600

    Don't use responsive images for icons and logos

--- components/ImageIcon.js
+++ components/ImageIcon.js
@@ -1,11 +1,5 @@
 // @flow
 import React from 'react'
-import css from '../styles/css'
-
-const style = css({
-  maxWidth: '100%',
-  height: 'auto',
-})
 
 type Props = {
   alt: string,
@@ -14,7 +8,6 @@ type Props = {
 export default (props: Props) => (
   <img
     alt={props.alt}
-    className={style}
     width={32}
     height={32}
     {...props}
